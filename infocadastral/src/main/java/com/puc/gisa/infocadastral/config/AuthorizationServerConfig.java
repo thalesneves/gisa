79c63b4fe8ee70354646908940b8d10e3290900f
Thales Neves <thales_neves97@hotmail.com>
2021-09-12 21:53:13 -0300

    feat: add OAuth2 in api info-cadastral

---
+++ infocadastral/src/main/java/com/puc/gisa/infocadastral/config/AuthorizationServerConfig.java
@@ -0,0 +1,45 @@
+package com.puc.gisa.infocadastral.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
+
+@Configuration
+@EnableAuthorizationServer
+public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+	
+	@Autowired
+	@Qualifier("authenticationManagerBean")
+	private AuthenticationManager authenticationManager;
+	
+	@Override
+	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+		clients.inMemory()
+			.withClient("angular")
+			.secret("{noop}@ngul@r0")
+			.scopes("read", "write")
+			.authorizedGrantTypes("password")
+			.accessTokenValiditySeconds(1800);
+	}
+	
+	@Override
+	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+		endpoints
+			.tokenStore(tokenStore())
+			.authenticationManager(this.authenticationManager);
+	}
+	
+	@Bean
+	public TokenStore tokenStore() {
+		return new InMemoryTokenStore(); 
+	}
+
+}
--- infocadastral/src/main/java/com/puc/gisa/infocadastral/controller/InfoCadastralController.java
+++ infocadastral/src/main/java/com/puc/gisa/infocadastral/controller/InfoCadastralController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.puc.gisa.infocadastral.dto.StakeHolderDTO;
 import com.puc.gisa.infocadastral.service.InfoCadastralService;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/infocadastral")
 public class InfoCadastralController {
